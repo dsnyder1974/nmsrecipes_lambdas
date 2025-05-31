c26eba0a3b5240b28b35e671b881433116d54e0a
David G Snyder <dsnyder74@gmail.com>
2025-05-31 18:46:44 -0400

    Added POST lambda for adding new categories.

---
+++ pgCategory/pgPostCategory/index.mjs
@@ -0,0 +1,64 @@
+import { getDbClient } from '/opt/nodejs/db.mjs';
+import { withCors } from '/opt/nodejs/withCors.mjs';
+
+// export async function handler(event) {
+const mainHandler = async (event) => {
+  const client = await getDbClient();
+
+  let body;
+
+  try {
+    body = JSON.parse(event.body);
+  } catch (err) {
+    console.error('Error parsing body:', err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Invalid request body' }),
+    };
+  }
+
+  try {
+    const { name, image } = body;
+
+    if (!name) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'name is required' }),
+      };
+    }
+
+    const query = `
+      INSERT INTO category (name, image)
+      VALUES ($1, $2)
+      RETURNING *;
+    `;
+
+    const values = [name, image || null];
+
+    console.log('Query:', query, 'with values:', values);
+
+    await client.connect();
+
+    const result = await client.query(query, values);
+
+    return {
+      statusCode: 201, // Created
+      body: JSON.stringify(result.rows[0]),
+    };
+
+} catch (err) {
+    console.error('Error inserting record:', err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+        details: err.message,
+      }),
+    };
+  } finally {
+    await client.end();
+  }
+};
+
+export const handler = withCors(mainHandler);
