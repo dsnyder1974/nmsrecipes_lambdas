3047442663d43c0263c1eac3b4e3c371bdfb6433
David G Snyder <dsnyder74@gmail.com>
2025-06-08 08:27:41 -0400

    Added pgListBuff

---
+++ pgBuff/pgListBuffs/index.mjs
@@ -0,0 +1,34 @@
+import { getDbClient } from '/opt/nodejs/db.mjs';
+import { withCors } from '/opt/nodejs/withCors.mjs';
+
+// export async function handler(event) {
+const mainHandler = async (event) => {
+  const client = await getDbClient();
+
+  try {
+    const query = `
+      SELECT buff_id, name, description
+      FROM ingestor.Buff
+      ORDER BY name;
+    `;
+
+    await client.connect();
+
+    const res = await client.query(query);
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.rows),
+    };
+  } catch (err) {
+    console.error('Database error:', err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error', details: err.message }),
+    };
+  }
+};
+
+export const handler = mainHandler;
